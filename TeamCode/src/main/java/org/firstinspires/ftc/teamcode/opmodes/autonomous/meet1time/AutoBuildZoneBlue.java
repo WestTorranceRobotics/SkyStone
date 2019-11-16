b628f0b7cffabe5812a54efb462c956b4fa68c6a
Riyana Roy <riyanaro@gmail.com>
2019-11-16 10:44:40 -0800

    hot changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/meet1time/AutoBuildZoneBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/meet1time/AutoBuildZoneBlue.java
@@ -16,11 +16,20 @@ public class AutoBuildZoneBlue extends TimedAuto {
         backupGyro1 = hardwareMap.get(BNO055IMU.class, "imu1");
         backupGyro1.initialize(new BNO055IMU.Parameters());
         waitForStart();
-        timeInches(70, -70);
+        timeInches(0, -30);
         bot.foundationGrabber.setGrabbed(BOTH, true);
         sleep(2000);
         timeInches(0, 100);
         bot.foundationGrabber.setGrabbed(BOTH, false);
         sleep(2000);
+        boolean saw = false;
+        while (opModeIsActive() && !saw) {
+            if (bot.driveTrain.onBlueLine() || saw) {
+                bot.driveTrain.spinDrive(0, 0, 0);
+                saw = true;
+            } else {
+                bot.driveTrain.spinDrive(-0.5,0,0);
+            }
+        }
     }
 }
