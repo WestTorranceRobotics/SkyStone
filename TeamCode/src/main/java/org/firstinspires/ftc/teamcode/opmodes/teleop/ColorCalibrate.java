9cc1ba520195d947d72ee17dd3b0759633435172
Riyana Roy <riyanaro@gmail.com>
2019-11-16 09:20:56 -0800

    hot changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ColorCalibrate.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "color calibrate")
+public class ColorCalibrate extends OpMode {
+
+    RevColorSensorV3 color;
+
+    @Override
+    public void init() {
+        color = hardwareMap.get(RevColorSensorV3.class, "lineColor");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("red", color.red());
+        telemetry.addData("blue", color.blue());
+    }
+}
